b156abf4c51cc49910bfc2f28f8da74806cfe340
faruk2535 <farukonibudo@gmail.com>
2023-08-14 16:35:15 +0000

    new

--- models/base_models.py
+++ models/base_models.py
@@ -3,7 +3,7 @@
 
 import uuid
 from datetime import datetime
-from models import storage
+# from models import storage
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
@@ -28,16 +28,16 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            # storage.new(self)
         
     def __str__(self):
-        print("{} {} {}").format(type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
             
     def save(self):
         """updates the public instance attribute updated_at"""
 
         self.updated_at = datetime.now()
-        storage.save()
+        # storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
