65f61da92ecb33d87b7fe4e376a073160d2d38f7
faruk2535 <farukonibudo@gmail.com>
2023-08-15 20:39:21 +0000

    new

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""this is the base model for the airbnb clone"""
+"""This script is the base model"""
 
 import uuid
 from datetime import datetime
-# from models import storage
+from models import storage
+
 
 class BaseModel:
+
+    """Class from which all other classes will inherit"""
+
     def __init__(self, *args, **kwargs):
         """Initializes instance attributes
 
@@ -28,16 +32,19 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            # storage.new(self)
-        
+            storage.new(self)
+
     def __str__(self):
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
-            
+        """Returns official string representation"""
+
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
+
     def save(self):
         """updates the public instance attribute updated_at"""
 
         self.updated_at = datetime.now()
-        # storage.save()
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
